4f51f538c5d7bb60e6607e712051f6a60fe57806
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-05-03 15:09:19 +0100

    navlink for leaderboard

--- client/src/components/Leaderboard/index.js
+++ client/src/components/Leaderboard/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
+import {NavLink} from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function Leaderboard() {
   return (
     <div className="leaderboard">
-        <button className="leaderbutton">Leaderboard</button>
+      <NavLink to="/leaderboard">Leaderboard</NavLink>
+        {/* <button className="leaderbutton">Leaderboard</button> */}
     </div>
   )
 }
