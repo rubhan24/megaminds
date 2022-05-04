8f1e6e395779b2f8cb8a1fe35b38e7fc6f5ee898
Jaxsan Sivanesan <jaxsan.sivanesan@gmail.com>
2022-05-04 14:33:09 +0100

    Testing

---
+++ client/src/components/Form/Form.test.js
@@ -0,0 +1,23 @@
+import { default as Form } from '.';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('Form', () => {
+    let getResultMock;
+
+    beforeEach(() => {
+        getResultMock = jest.fn();
+        render(<Form getResult={getResultMock}/>);
+    });
+
+    test('it renders a form', () => {
+        let form = screen.getByRole('form');
+        expect(form).toBeInTheDocument();;
+    });
+
+    // test('it calls on getResult prop on form submission', () => {
+    //     let locationInput = screen.getByLabelText('Location');
+    //     userEvent.type(locationInput, "Hong Kong{enter}")
+    //     expect(getResultMock).toHaveBeenNthCalledWith(1, 'Hong Kong');
+    // })
+});
